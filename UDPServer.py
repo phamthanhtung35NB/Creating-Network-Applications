2b0894837fb1c9ff0057d4739e2b2c79a416f2cc
phamthanhtung35nb <tungpham010203@gmail.com>
2025-03-26 21:11:30 +0700

    f commmit

---
+++ UDPServer.py
@@ -0,0 +1,13 @@
+from socket import *
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))  # Lắng nghe trên tất cả các IP của máy
+
+print("The server is ready to receive...")
+
+while True:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage = message.decode().upper()
+    print(f"Received from {clientAddress}: {message.decode()}")
+    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
